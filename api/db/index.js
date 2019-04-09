4851eaf6ec27fdf03a1e54e3d8cbcc64033d03de
Malachy Gilchrist <malachygilchrist@gmail.com>
2019-04-09 16:22:05 +0100

    refactor db index

--- api/db/index.js
+++ api/db/index.js
@@ -2,15 +2,14 @@ const { Pool } = require('pg')
 
 const pool = new Pool()
 
+const query = async (text, params) => {
+    const start = Date.now()
+    const res = await pool.query(text, params)
+    const duration = Date.now() - start
+    console.log({text, duration, rowCount: res.rowCount})
+    return res
+}
+
 module.exports = {
-    query: async (text, params) => {
-        const start = Date.now()
-        const res = await pool.query(text, params)
-        const duration = Date.now() - start
-        console.log({text, duration, rowCount: res.rowCount})
-        return res
-    },
-    getClient: () => {
-        return pool.connect()
-    }
+    query
 }
